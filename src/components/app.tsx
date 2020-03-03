bd4cac7c21dfeaf45005197e6ba81435a0fdadb3
Embla <emblajonsson@gmail.com>
2020-03-03 11:25:33 +0100

    .

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import Layout from './layout';
+import Layout from './layout'
+import Spinner from './spinner'
+import { Suspense } from 'react'
 
 /**
  * Application top level component. This is a good place for future
  * top level components - ErrorBoundary, Provider & Suspense, etc.
  */
 export default function App() {
-
-    return (
-        <BrowserRouter>
-            <Layout/>
-        </BrowserRouter>
-    )
+  return (
+    <Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </Suspense>
+  )
 }
